0b1461754b3567fcd462520bd38ed887cb034ee3
millionhari <mikemhari@gmail.com>
2015-02-25 15:40:42 -0800

    Serve index.html and styles.css

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -13,6 +13,11 @@ exports.headers = headers = {
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  fs.readFile(asset, "binary", function(err, file){
+    res.writeHead(200);
+    res.write(file, "binary");
+    callback();
+  })
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,49 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var urlParser = require('url');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
+exports.router = {
+  GET: function(req, res){
+    // get route from URL
+    var parts = urlParser.parse(req.url);
+    var route = parts.pathname;
+    // if '/'
+    if (route === '/'){
+      //serve up to index.html
+      var index = archive.paths.siteAssets.concat('/index.html');
+      httpHelpers.serveAssets(res, index, function(){
+        res.end();
+      });
+    }
+    if (route === '/styles.css'){
+      var style = archive.paths.siteAssets.concat('/styles.css');
+      httpHelpers.serveAssets(res, style, function(){
+        res.end();
+      });
+    }
+    // look up website in sites.txt
+    // if exists in archive
+      //serve it up
+    // else serve up to loading.html
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  exports.router[req.method](req, res);
+
+
+  // handle GET requests
+  // if(req.method === 'GET'){
+
+  //   // respond with homepage
+  //   if(req.url === '/'){
+  //     console.log('the url', req.url);
+
+  //   }
+  // }
+  // res.end(archive.paths.list);
+
 };
